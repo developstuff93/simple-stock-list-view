dbebdda38eb6def4169e2f6ebd31e9cbee22403c
Jose Arias <developstuff93@gmail.com>
2021-09-06 14:47:39 -0400

    integrated routes and stock components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,35 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 import { Route, Switch } from 'react-router-dom'
+import AppStateProvider from '../Hooks/AppStateProvider'
 
 import Home from './Home'
 import Nav from './Nav'
+import Stock from './Stock'
 
 const App = () => {
   return (
-    <div
-      css={css`
-        background: #fff;
-        box-sizing: border-box;
-        color: #34374c;
-        min-height: 100%;
-        padding: 20px 80px;
-      `}
-    >
-      <Nav />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
+    <AppStateProvider>
+      <div
+        css={css`
+          background: #fff;
+          box-sizing: border-box;
+          color: #34374c;
+          min-height: 100%;
+          padding: 20px 80px;
+        `}
+      >
+        <Nav />
+        <Switch>
+          <Route exact path="/stocks/:symbol">
+            <Stock />
+          </Route>
+          <Route exact path={['/stocks', '/']}>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </AppStateProvider>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,22 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
+import Stocks from './Stocks'
 
-const Home = ({ stocks = [] }) => (
-  <div>
-    <h1
-      css={css`
-        font-size: 48px;
-        font-weight: 500;
-        line-height: 60px;
-      `}
-    >
-      Stocks
-    </h1>
-    {stocks.length ? (
-      <ul
+const Home = () => {
+  return (
+    <div>
+      <h1
         css={css`
-          padding: 0;
+          font-size: 48px;
+          font-weight: 500;
+          line-height: 60px;
         `}
-      ></ul>
-    ) : null}
-  </div>
-)
+      >
+        Stocks
+      </h1>
+      <Stocks />
+    </div>
+  )
+}
 
 export default Home
